4f7a32835b6fa0c7bd21893f77b6ff55a9e8642a
MadoXZA <timklo15@gmail.com>
2024-01-03 06:47:53 +0000

    Task completed

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,7 @@
+// Wrote a export function named loadBalancer
+// It should accept two arguments chinaDownload (Promise)
+// and USDownload (Promise).
+
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]);
+}
